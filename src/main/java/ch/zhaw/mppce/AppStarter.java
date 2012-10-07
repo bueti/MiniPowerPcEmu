ffd7af49a0e2205d9892f3cfbcf6d88d107925ae
bueti <bueti@offroadfreunde.ch>
2012-10-07 16:27:22 +0200

    AppStarter hinzugefügt

---
+++ src/main/java/ch/zhaw/mppce/AppStarter.java
@@ -0,0 +1,14 @@
+package ch.zhaw.mppce;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bbu
+ * Date: 07.10.12
+ * Time: 16:23
+ */
+public class AppStarter {
+
+    public static void main(String[] args) {
+        CPU cpu = new CPU();
+    }
+}
