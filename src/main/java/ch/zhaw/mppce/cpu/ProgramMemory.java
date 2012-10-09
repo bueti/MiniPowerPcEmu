a6d11ab40c65fe2a6691d544cddca6d471c90310
bueti <bueti@offroadfreunde.ch>
2012-10-09 22:41:31 +0200

    Revert "deleted files hergestellt"
    This reverts commit 91ff4f3374e822176ddec9e6ebf7e7d54e81164c.

---
+++ src/main/java/ch/zhaw/mppce/cpu/ProgramMemory.java
@@ -0,0 +1,44 @@
+package ch.zhaw.mppce.cpu;
+
+import ch.zhaw.mppce.compiler.instructions.Instruction;
+
+import java.util.HashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bbu
+ * Date: 07.10.12
+ * Time: 15:47
+ */
+public class ProgramMemory extends Memory {
+
+    private HashMap<Integer, Instruction> programMemory;
+
+    public ProgramMemory() {
+        programMemory = new HashMap<Integer, Instruction>();
+    }
+
+    /**
+     * Store a given command
+     *
+     * @param address   Address of command
+     * @param command   commando
+     *
+     */
+    public void storeCommand(int address, Instruction command) {
+        programMemory.put(address, command);
+
+    }
+
+    /**
+     * Get given command by address
+     *
+     * @param address
+     * @return command
+     *
+     */
+    public Instruction getCommand(int address) {
+        return programMemory.get(address);
+
+    }
+}
