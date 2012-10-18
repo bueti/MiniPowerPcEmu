aa22cb6e8cd804e4c9b9493553a51f72a7235a3a
bueti <bueti@offroadfreunde.ch>
2012-10-18 19:09:53 +0200

    INC implementiert, overflow check fehlt.

--- src/main/java/ch/zhaw/mppce/compiler/instructions/INC.java
+++ src/main/java/ch/zhaw/mppce/compiler/instructions/INC.java
@@ -2,6 +2,7 @@ package ch.zhaw.mppce.compiler.instructions;
 
 import ch.zhaw.mppce.cpu.Memory;
 import ch.zhaw.mppce.cpu.Register;
+import ch.zhaw.mppce.tools.Tools;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,10 +14,27 @@ public class INC extends Instruction {
 
     @Override
     public void doIt(Memory programMemory, Memory dataMemory, Register accu, Register register1, Register register2, Register register3) {
+        Tools tools = new Tools();
+
+        // Get Value in Accumulator);
+        String accuValue = accu.getRegister();
+
+        // TODO: Check for overflow and set carry bit.
+        // Convert to Dec
+        int a = Integer.parseInt(accuValue);
+
+        // +1
+        a++;
+
+        // Convert to Twos Complement
+        accuValue = tools.twoComplement(a);
+
+        // Save
+        accu.setRegister(accuValue);
     }
 
     @Override
     public String convertToOpcode() {
-        return "0000000100000000";
+        return "00000001--------";
     }
 }
