7a4720ef35b14ce0ce73d558f542a24dda87b53d
bueti <bueti@offroadfreunde.ch>
2012-10-18 19:17:25 +0200

    DEC implementiert

--- src/main/java/ch/zhaw/mppce/compiler/instructions/DEC.java
+++ src/main/java/ch/zhaw/mppce/compiler/instructions/DEC.java
@@ -2,6 +2,7 @@ package ch.zhaw.mppce.compiler.instructions;
 
 import ch.zhaw.mppce.cpu.Memory;
 import ch.zhaw.mppce.cpu.Register;
+import ch.zhaw.mppce.tools.Tools;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,13 +12,40 @@ import ch.zhaw.mppce.cpu.Register;
  */
 public class DEC extends Instruction {
 
+    // TODO: TEST!!!!
     @Override
     public void doIt(Memory programMemory, Memory dataMemory, Register accu, Register register1, Register register2, Register register3) {
+        Tools tools = new Tools();
+        Boolean overflow = false;
+
+        // Get Value in Accumulator);
+        String accuValue = accu.getRegister();
+
+        // Convert to Dec
+        int a = Integer.parseInt(accuValue);
+
+        // Check for overflow
+        if (a <= -65536) {
+            overflow = true;
+        }
+
+        // -1
+        a--;
+
+        // Convert to Twos Complement
+        accuValue = tools.twoComplement(a);
+
+        // Save
+        accu.setRegister(accuValue);
+
+        // Set carry bit if necessary
+        if (overflow)
+            accu.setCarryBit();
     }
 
     @Override
 
     public String convertToOpcode() {
-        return "0000010000000000";
+        return "00000100--------";
     }
 }
