c779e549d3ff0320cc00b09de59733a612900c13
bueti <bueti@offroadfreunde.ch>
2012-10-19 20:13:27 +0200

    SLA implementiert

---
+++ src/main/java/ch/zhaw/mppce/compiler/instructions/SLA.java
@@ -0,0 +1,29 @@
+package ch.zhaw.mppce.compiler.instructions;
+
+import ch.zhaw.mppce.cpu.Memory;
+import ch.zhaw.mppce.cpu.Register;
+import ch.zhaw.mppce.tools.Tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bbu
+ * Date: 19.10.12
+ * Time: 20:11
+ */
+public class SLA extends Instruction {
+    @Override
+    public String convertToOpcode() {
+        return "00001000--------";
+    }
+
+    @Override
+    public void doIt(Memory programMemory, Memory dataMemory, Register accu, Register register1, Register register2, Register register3) {
+        Tools tools = new Tools();
+
+        String accuVal = accu.getRegister();
+        int accuValDec = tools.convertToDec(accuVal);
+
+        accu.setRegister(tools.convertToBin(accuValDec * 2));
+
+    }
+}
