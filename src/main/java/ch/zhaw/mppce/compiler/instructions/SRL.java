a4017572bceabe1dcdddf9abaae83d60de53c9da
bueti <bueti@offroadfreunde.ch>
2012-10-19 20:47:25 +0200

    SRL implementiert

---
+++ src/main/java/ch/zhaw/mppce/compiler/instructions/SRL.java
@@ -0,0 +1,31 @@
+package ch.zhaw.mppce.compiler.instructions;
+
+import ch.zhaw.mppce.cpu.Memory;
+import ch.zhaw.mppce.cpu.Register;
+import ch.zhaw.mppce.tools.Tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bbu
+ * Date: 19.10.12
+ * Time: 20:46
+ */
+public class SRL extends Instruction {
+
+    @Override
+    public String convertToOpcode() {
+        return "00001001--------";
+    }
+
+    @Override
+    public void doIt(Memory programMemory, Memory dataMemory, Register accu, Register register1, Register register2, Register register3) {
+        Tools tools = new Tools();
+
+        String accuVal = accu.getRegister();
+        int accuValDec = tools.convertToDec(accuVal);
+
+        accu.setRegister(tools.convertToBin(accuValDec / 2));
+
+        // TODO: MSB Handling
+    }
+}
