2bb6c560e00b36af918c0da2e62f89678fa9f586
bueti <bueti@offroadfreunde.ch>
2012-10-20 16:08:54 +0200

    BCD implementiert

---
+++ src/main/java/ch/zhaw/mppce/compiler/instructions/BCD.java
@@ -0,0 +1,37 @@
+package ch.zhaw.mppce.compiler.instructions;
+
+import ch.zhaw.mppce.cpu.CPU;
+import ch.zhaw.mppce.cpu.Memory;
+import ch.zhaw.mppce.cpu.Register;
+import ch.zhaw.mppce.tools.Tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bbu
+ * Date: 20.10.12
+ * Time: 16:04
+ */
+public class BCD extends Instruction {
+    @Override
+    public String convertToOpcode(Memory dataMemory) {
+        Tools tools = new Tools();
+        String address = tools.convertToBin(tools.getAddressFromParams(getParameters()));
+
+        return "00111" + address;
+    }
+
+    @Override
+    public void doIt(CPU cpu) {
+        Tools tools = new Tools();
+        String address = tools.convertToBin(tools.getAddressFromParams(getParameters()));
+        Register accu = cpu.getAccu();
+
+        if (accu.hasCarryBit()) {
+            // branch to address
+            cpu.setCommandPointer(tools.getAddressFromParams(getParameters()));
+        } else {
+            cpu.incCommandPointer();
+        }
+    }
+
+}
