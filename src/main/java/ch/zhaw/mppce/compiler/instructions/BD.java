d6d803481e1b853e4f722c69a1cfa8c5b53e3ceb
bueti <bueti@offroadfreunde.ch>
2012-10-20 16:11:42 +0200

    BD implementiert

---
+++ src/main/java/ch/zhaw/mppce/compiler/instructions/BD.java
@@ -0,0 +1,34 @@
+package ch.zhaw.mppce.compiler.instructions;
+
+import ch.zhaw.mppce.cpu.CPU;
+import ch.zhaw.mppce.cpu.Memory;
+import ch.zhaw.mppce.cpu.Register;
+import ch.zhaw.mppce.tools.Tools;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bbu
+ * Date: 20.10.12
+ * Time: 16:10
+ */
+public class BD extends Instruction {
+
+    @Override
+    public String convertToOpcode(Memory dataMemory) {
+        Tools tools = new Tools();
+        String address = tools.convertToBin(tools.getAddressFromParams(getParameters()));
+
+        return "00100" + address;
+
+    }
+
+    @Override
+    public void doIt(CPU cpu) {
+        Tools tools = new Tools();
+        String address = tools.convertToBin(tools.getAddressFromParams(getParameters()));
+        Register accu = cpu.getAccu();
+
+        // branch to address
+        cpu.setCommandPointer(tools.getAddressFromParams(getParameters()));
+    }
+}
